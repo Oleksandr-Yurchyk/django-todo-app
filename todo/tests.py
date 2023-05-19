03b385707e54809109f615e91b82339e72d0d3d4
Oleksandr Yurchyk <yurchyk_sasha@ukr.net>
2023-05-19 11:16:03 +0300

    added simple test

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+
+@pytest.mark.parametrize(
+    'text_input, result', [('5+5', 10), ('1+4', 5)]
+)
+def test_sum(text_input, result):
+    assert eval(text_input) == result
